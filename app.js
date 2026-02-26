f948c238bcac31518b993d470f4683fd722aef81
arthurmarques0071-hue <arthurmarques0071@gmail.com>
2026-02-25 21:54:43 -0300

    inicializando server

---
+++ app.js
@@ -0,0 +1,30 @@
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', async function handler (request, reply) {
+ 
+  const users = [
+    {
+        nome: "arthur",
+        idade: 18
+        
+    
+    }
+    
+];
+    
+  
+
+})
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
